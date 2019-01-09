0f2e39c2bd413f0a6cf3d736b29f4e62085cb7f2
gengmingming <gengming3678@qq.com>
2019-01-09 14:12:06 +0800

    添加2行输出

--- back_order_1001/src/back_order_1001/Test1001.java
+++ back_order_1001/src/back_order_1001/Test1001.java
@@ -5,6 +5,7 @@ public class Test1001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("0000000000");
+          System.out.println("0000000000");
 	}
 
 }
