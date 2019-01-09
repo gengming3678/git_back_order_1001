cafa76af6a721a16aff76590586df08a8c1fb44e
gengmingming <gengming3678@qq.com>
2019-01-09 14:34:31 +0800

    11112

--- back_order_1001/src/back_order_1001/Test1001.java
+++ back_order_1001/src/back_order_1001/Test1001.java
@@ -9,6 +9,7 @@ public class Test1001 {
           System.out.println("0000000000");
           System.out.println("0000000000");
           System.out.println("0000000000");
+          System.out.println("0000000000");
 	}
 
 }
